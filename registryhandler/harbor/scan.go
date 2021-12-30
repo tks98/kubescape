8d8a574a8519e93518cb70fd471771747c82814d
Travis Smith <travis.kenneth.smith@gmail.com>
2021-12-29 23:35:04 -0600

    implement scanall method and add test

--- registryhandler/harbor/scan.go
+++ registryhandler/harbor/scan.go
@@ -3,8 +3,11 @@ package harbor
 import (
 	"context"
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/scan"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/scan_all"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
 )
 
+// ScanImage sends a scan request for the specified image
 func (h harborRegistry) ScanImage(projectName, repositoryName, referenceName string) (*scan.ScanArtifactAccepted, error) {
 
 	params := scan.ScanArtifactParams{
@@ -21,4 +24,19 @@ func (h harborRegistry) ScanImage(projectName, repositoryName, referenceName str
 
 	return result, nil
 
+}
+
+// ScanAll sends a manual request to scan all images in the registry
+func (h harborRegistry) ScanAll() (*scan_all.CreateScanAllScheduleCreated, error) {
+
+	params := scan_all.CreateScanAllScheduleParams{
+		Schedule: &models.Schedule{Schedule: &models.ScheduleObj{Type: models.ScheduleObjTypeManual}},
+	}
+
+	schedule, err := h.client.V2().ScanAll.CreateScanAllSchedule(context.Background(), &params)
+	if err != nil {
+		return nil, err
+	}
+
+	return schedule, err
 }
